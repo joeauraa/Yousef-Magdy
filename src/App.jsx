5597417fa990ef91008265c0c91314fbcd88d04d
Yousef Magdy <yousefmagdyhassan@gmail.com>
2025-09-01 14:35:01 -0700

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2025{" "}
-                <a href="https://www.linkedin.com/in/yash-verma-46b95b238" className="hover:underline" target="_blank" rel="noopener noreferrer">
+                <a href="" className="hover:underline" target="_blank" rel="noopener noreferrer">
                   Yousef Magdy™
                 </a>
                 . All Rights Reserved.
@@ -57,7 +57,7 @@ const ProjectPageLayout = () => (
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
           © 2025{" "}
-          <a href="https://www.linkedin.com/in/yash-verma-46b95b238" className="hover:underline" target="_blank" rel="noopener noreferrer">
+          <a href="h" className="hover:underline" target="_blank" rel="noopener noreferrer">
             Yousef Magdy™
           </a>
           . All Rights Reserved.
